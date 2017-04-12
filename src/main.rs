89b906cf5b1537c7e8746bbd748e457ed46e05ea
uint256_t <konndennsa@gmail.com>
2017-04-12 18:23:25 +0900

    update process of cmd line args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod version_info;
 
 fn main() {
-    version_info::show_version();
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        version_info::show_version();
+    } else {
+        let input_file_name = args[1].to_string(); // is this correct?
+        // TODO: pass lexer input_file_name
+    }
 }
