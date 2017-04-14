c7c793a87567d5a096f70e20551e36938981207b
uint256_t <konndennsa@gmail.com>
2017-04-14 16:57:38 +0900

    lexer can run a little

--- src/lexer.rs
+++ src/lexer.rs
@@ -11,13 +11,24 @@ pub enum TokenKind {
     String,
     Char,
     Symbol,
+    Newline,
     End,
 }
 
 pub struct Token {
-    kind: TokenKind,
-    val: String,
-    line: i32,
+    pub kind: TokenKind,
+    pub val: String,
+    pub line: i32,
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, val: &str, line: i32) -> Token {
+        Token {
+            kind: kind,
+            val: val.to_string(),
+            line: line,
+        }
+    }
 }
 
 pub struct Lexer<'a> {
@@ -38,15 +49,58 @@ impl<'a> Lexer<'a> {
     pub fn get_filename(self) -> String {
         self.filename
     }
-    pub fn read_token(&mut self) -> Token {
-        // TODO: tentatively
-        println!("{}", self.peek.peek().unwrap());
-        self.peek.next();
 
-        Token {
-            kind: TokenKind::End,
-            val: "".to_string(),
-            line: 0,
+    pub fn read_identifier(&mut self) -> Token {
+        let mut ident = String::new();
+        loop {
+            match self.peek.peek() {
+                Some(&c) => {
+                    match c {
+                        'a'...'z' | 'A'...'Z' | '_' | '0'...'9' => ident.push(c),
+                        _ => break,
+                    }
+                }
+                _ => break,
+            };
+            self.peek.next();
         }
+        Token {
+            kind: TokenKind::Identifier,
+            val: ident,
+            line: self.cur_line,
+        }
+    }
+    pub fn read_newline(&mut self) -> Token {
+        self.peek.next();
+        Token {
+            kind: TokenKind::Newline,
+            val: "".to_string(),
+            line: self.cur_line,
+        }
+    }
+    pub fn read_symbol(&mut self) -> Token {
+        let c = self.peek.next();
+        let mut sym = String::new();
+        sym.push(c.unwrap());
+        Token::new(TokenKind::Symbol, sym.as_str(), self.cur_line)
+    }
+
+    pub fn read_token(&mut self) -> Option<Token> {
+        // TODO: tentatively
+        let ret = match self.peek.peek() {
+            Some(&c) => {
+                match c {
+                    'a'...'z' | 'A'...'Z' | '_' => Some(self.read_identifier()),
+                    ' ' | '\t' => {
+                        self.peek.next();
+                        self.read_token()
+                    }
+                    '\n' => Some(self.read_newline()),
+                    _ => Some(self.read_symbol()),
+                }
+            }
+            None => None as Option<Token>,
+        };
+        ret
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ fn main() {
         version_info::show_usage();
     } else {
         let input_file_name = args[1].to_string(); // is this correct?
-        // TODO: pass lexer input_file_name
         let mut file = OpenOptions::new()
             .read(true)
             .open(input_file_name.to_string())
@@ -24,9 +23,17 @@ fn main() {
         let mut s = String::new();
         file.read_to_string(&mut s);
         let mut lexer = lexer::Lexer::new(input_file_name, s.as_str());
+
         // test
-        lexer.read_token();
-        lexer.read_token();
-        lexer.read_token();
+        let mut tok: Option<lexer::Token>;
+        loop {
+            tok = lexer.read_token();
+            match tok {
+                Some(t) => {
+                    println!("token: {}", t.val);
+                }
+                None => break,
+            }
+        }
     }
 }
