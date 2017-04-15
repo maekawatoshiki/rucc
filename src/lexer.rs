26cbf2df3372f76a6a2d4213da990efbdfd1b786
uint256_t <konndennsa@gmail.com>
2017-04-15 12:02:04 +0900

    lexer update

--- src/lexer.rs
+++ src/lexer.rs
@@ -71,7 +71,12 @@ impl<'a> Lexer<'a> {
     }
     fn peek_next_char_is(&mut self, ch: char) -> bool {
         let nextc = self.peek_next();
+        self.peek_unget(nextc);
         nextc == ch
+    }
+    fn peek_char_is(&mut self, ch: char) -> bool {
+        let peekc = self.peek_get().unwrap();
+        *peekc == ch
     }
 
     pub fn read_identifier(&mut self) -> Token {
@@ -90,6 +95,32 @@ impl<'a> Lexer<'a> {
         }
         Token::new(TokenKind::Identifier, ident.as_str(), self.cur_line)
     }
+    fn read_number_literal(&mut self) -> Token {
+        let mut num = String::new();
+        let mut is_float = false;
+        loop {
+            match self.peek_get() {
+                Some(&c) => {
+                    match c {
+                        '.' | '0'...'9' => {
+                            num.push(c);
+                            if (c == '.') {
+                                is_float = true;
+                            }
+                        }
+                        _ => break,
+                    }
+                }
+                _ => break,
+            };
+            self.peek_next();
+        }
+        if is_float {
+            Token::new(TokenKind::FloatNumber, num.as_str(), self.cur_line)
+        } else {
+            Token::new(TokenKind::IntNumber, num.as_str(), self.cur_line)
+        }
+    }
     pub fn read_newline(&mut self) -> Token {
         self.peek_next();
         Token::new(TokenKind::Newline, "", self.cur_line)
@@ -98,10 +129,50 @@ impl<'a> Lexer<'a> {
         let c = self.peek_next();
         let mut sym = String::new();
         sym.push(c);
+        match c {
+            '+' | '-' | '*' | '/' | '%' | '=' | '^' | '!' => {
+                if (self.peek_char_is('=')) {
+                    sym.push(self.peek_next());
+                }
+            }
+            '<' | '>' | '&' | '|' => {
+                if (self.peek_char_is(c)) {
+                    sym.push(self.peek_next());
+                }
+                if (self.peek_char_is('=')) {
+                    sym.push(self.peek_next());
+                }
+            }
+            '.' => {
+                if (self.peek_char_is('.') && self.peek_next_char_is('.')) {
+                    sym.push(self.peek_next());
+                    sym.push(self.peek_next());
+                }
+            }
+            _ => {}
+        };
         Token::new(TokenKind::Symbol, sym.as_str(), self.cur_line)
     }
+    fn read_string_literal(&mut self) -> Token {
+        self.peek_next();
+        let mut s = String::new();
+        while !self.peek_char_is('\"') {
+            s.push(self.peek_next());
+        }
+        self.peek_next();
+        Token::new(TokenKind::String, s.as_str(), self.cur_line)
+    }
+    fn read_char_literal(&mut self) -> Token {
+        self.peek_next();
+        let mut s = String::new();
+        while !self.peek_char_is('\'') {
+            s.push(self.peek_next());
+        }
+        self.peek_next();
+        Token::new(TokenKind::Char, s.as_str(), self.cur_line)
+    }
 
-    pub fn read_token(&mut self) -> Option<Token> {
+    pub fn do_read_token(&mut self) -> Option<Token> {
         match self.peek_get() {
             Some(&c) => {
                 match c {
@@ -110,11 +181,26 @@ impl<'a> Lexer<'a> {
                         self.peek_next();
                         self.read_token()
                     }
+                    '0'...'9' => Some(self.read_number_literal()),
+                    '\"' => Some(self.read_string_literal()),
+                    '\'' => Some(self.read_char_literal()),
                     '\n' => Some(self.read_newline()),
                     _ => Some(self.read_symbol()),
                 }
             }
             None => None as Option<Token>,
+        }
+    }
+    pub fn read_token(&mut self) -> Option<Token> {
+        let t = self.do_read_token();
+        match t {
+            Some(tok) => {
+                match tok.kind {
+                    TokenKind::Newline => self.read_token(),
+                    _ => Some(tok),
+                }
+            }
+            _ => t,
         }
     }
 }
